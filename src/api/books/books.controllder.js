bf17f6a16b4523e4d177fe8f3b1bc23e22b365df
yisu-kim <yisu.kim.dev@outlook.com>
2020-09-04 23:43:54 +0900

    [Feat] Book 데이터 생성과 조회 기능

--- src/api/books/books.controllder.js
+++ src/api/books/books.controllder.js
@@ -1,9 +1,60 @@
-export const list = (ctx) => {
-  ctx.body = "listed";
+import Book from "models/Book";
+
+export const create = async (ctx) => {
+  const { title, authors, publishedDate, price, tags } = ctx.request.body;
+
+  const book = new Book({
+    title,
+    authors,
+    publishedDate,
+    price,
+    tags,
+  });
+
+  try {
+    await book.save();
+  } catch (e) {
+    return ctx.throw(500, e);
+  }
+
+  ctx.body = book;
 };
 
-export const create = (ctx) => {
-  ctx.body = "created";
+export const list = async (ctx) => {
+  let books;
+
+  try {
+    books = await Book.find().sort({ _id: -1 }).limit(3);
+  } catch (e) {
+    return ctx.throw(500, e);
+  }
+
+  ctx.body = books;
+};
+
+export const get = async (ctx) => {
+  const { id } = ctx.params;
+
+  let book;
+
+  console.log("get method");
+  try {
+    book = await Book.findById(id);
+  } catch (e) {
+    if (e.name === "CastError") {
+      ctx.status = 400;
+      return;
+    }
+    return ctx.throw(500, e);
+  }
+
+  if (!book) {
+    ctx.status = 400;
+    ctx.body = { message: "book not found" };
+    return;
+  }
+
+  ctx.body = book;
 };
 
 export const del = (ctx) => {
--- src/api/books/index.js
+++ src/api/books/index.js
@@ -1,9 +1,10 @@
 import Router from "@koa/router";
-import { list, create, del, replace, update } from "./books.controllder";
+import { list, get, create, del, replace, update } from "./books.controllder";
 
 const books = new Router();
 
 books.get("/", list);
+books.get("/:id", get);
 
 books.post("/", create);
 
